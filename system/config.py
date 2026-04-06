e0a2ad0b9393224fb984b122348534596b6609cb
huyvuong1109 <huyvq119@gmail.com>
2026-04-07 01:39:57 +0700

    thay vị trí

--- system/config.py
+++ system/config.py
@@ -2,6 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# Tự động load từ Kaggle Secrets nếu đang chạy trên Kaggle
+try:
+    from kaggle_secrets import UserSecretsClient
+    secrets = UserSecretsClient()
+    os.environ["OPENROUTER_API_KEY"] = secrets.get_secret("openrouterapi")
+except Exception:
+    pass  # Không phải môi trường Kaggle, bỏ qua
+
 class Settings:
     openrouter_api_key: str = os.getenv("OPENROUTER_API_KEY", "")
     llm_model: str = os.getenv(
